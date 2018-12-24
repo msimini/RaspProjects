4798884fa0052ffc3e571872fea8022ca3e032dc
msimini <mako1393@gmail.com>
2018-12-24 15:37:50 -0200

    mm

--- 14_distance.py
+++ 14_distance.py
@@ -3,24 +3,24 @@ import RPi.GPIO as GPIO
 import time
 
 def checkdist():
-	GPIO.output(29, GPIO.HIGH)
+	GPIO.output(31, GPIO.HIGH)
 	time.sleep(0.000015)
-	GPIO.output(29, GPIO.LOW)
-	while not GPIO.input(31):
+	GPIO.output(31, GPIO.LOW)
+	while not GPIO.input(29):
 		pass
 	t1 = time.time()
-	while GPIO.input(31):
+	while GPIO.input(29):
 		pass
 	t2 = time.time()
 	return (t2-t1)*340/2
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(29,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(31,GPIO.IN)
+GPIO.setup(31,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(29,GPIO.IN)
 time.sleep(2)
 try:
 	while True:
-		print 'Distance: %0.2f m' %checkdist()
+		print ('Distance: %0.2f m' %checkdist())
 		time.sleep(0.5)
 except KeyboardInterrupt:
 	GPIO.cleanup()
